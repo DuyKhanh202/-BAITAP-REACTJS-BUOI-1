a6736d0433887a6efce083feb2ab05e98ad1e2a4
DuyKhanh202 <lenguyenduykhanh02@gmail.com>
2024-08-28 17:53:17 +0700

    Bài tập buổi 31

--- src/components/item.js
+++ src/components/item.js
@@ -8,47 +8,72 @@ const Item = () => {
             <section className="pt-4">
                 <div className="container px-lg-5">
                     <div className="row gx-lg-5">
-                        <div className="col-md-3 mb-5">
+                        <div className="col-lg-6 col-xxl-4 mb-5">
                             <div className="card bg-light border-0 h-100">
-                                <img className="card-img-top" src="https://i.pinimg.com/originals/ce/7f/55/ce7f55d0ce9ec88c238f4f0f5731d809.jpg" alt="Card image cap" />
                                 <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <h5 class="card-title mt-1">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i className="bi bi-collection"></i>
+                                    </div>
+                                    <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+                                    <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-3 mb-5">
+                        <div className="col-lg-6 col-xxl-4 mb-5">
                             <div className="card bg-light border-0 h-100">
-                                <img className="card-img-top" src="https://i.pinimg.com/originals/ce/7f/55/ce7f55d0ce9ec88c238f4f0f5731d809.jpg" alt="Card image cap" />
                                 <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <h5 class="card-title mt-1">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i className="bi bi-cloud-download"></i>
+                                    </div>
+                                    <h2 className="fs-4 fw-bold">Free to download</h2>
+                                    <p className="mb-0">As always, Start Bootstrap has a powerful collection of free templates.</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-3 mb-5">
+                        <div className="col-lg-6 col-xxl-4 mb-5">
                             <div className="card bg-light border-0 h-100">
-                                <img className="card-img-top" src="https://i.pinimg.com/originals/ce/7f/55/ce7f55d0ce9ec88c238f4f0f5731d809.jpg" alt="Card image cap" />
                                 <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <h5 class="card-title mt-1">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i className="bi bi-card-heading"></i>
+                                    </div>
+                                    <h2 className="fs-4 fw-bold">Jumbotron hero header</h2>
+                                    <p className="mb-0">The heroic part of this template is the jumbotron hero header!</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-3 mb-5">
+                        <div className="col-lg-6 col-xxl-4 mb-5">
                             <div className="card bg-light border-0 h-100">
-                                <img className="card-img-top" src="https://i.pinimg.com/originals/ce/7f/55/ce7f55d0ce9ec88c238f4f0f5731d809.jpg" alt="Card image cap" />
                                 <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <h5 className="card-title mt-1">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" className="btn btn-primary">Find Out More!</a>
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i className="bi bi-bootstrap"></i>
+                                    </div>
+                                    <h2 className="fs-4 fw-bold">Feature boxes</h2>
+                                    <p className="mb-0">We've created some custom feature boxes using Bootstrap icons!</p>
                                 </div>
                             </div>
                         </div>
-
+                        <div className="col-lg-6 col-xxl-4 mb-5">
+                            <div className="card bg-light border-0 h-100">
+                                <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i className="bi bi-code"></i>
+                                    </div>
+                                    <h2 className="fs-4 fw-bold">Simple clean code</h2>
+                                    <p className="mb-0">We keep our dependencies up to date and squash bugs as they come!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-xxl-4 mb-5">
+                            <div className="card bg-light border-0 h-100">
+                                <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i className="bi bi-patch-check"></i>
+                                    </div>
+                                    <h2 className="fs-4 fw-bold">A name you trust</h2>
+                                    <p className="mb-0">Start Bootstrap has been the leader in free Bootstrap templates since 2013!</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
